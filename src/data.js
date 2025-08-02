9ce6ad47c337c09ca429c2c729c80515cba3244d
irfan-za <fanzahran123@gmail.com>
2025-08-02 19:07:57 +0700

    fix: music data

--- src/data.js
+++ src/data.js
@@ -2,21 +2,23 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Under the City Stars",
+      name: "Daydream",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        "https://cms.chillhop.com/media/81732/squarel8517eb515710b5f0f8efd83d1f17ae813cbb13be.jpg",
       artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/330/208/main-version/streaming-previews/STREAMING-daydream-mood-maze-main-version-02-35-1035.mp3",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Daylight",
+      name: "Lazy Sunday",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+        "https://cms.chillhop.com/media/20525/squarel882941246e3074c7e591043dd0d8aae7e16c476d.jpg",
       artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/6fbc86c8cdaa77dfd625df295f75e965/13ed17301dd3744e5a15f73abd959942/e2a1c798056d34b3699d64a5464dab4f/STREAMING-lazy-sunday-hybridas-main-version-38210-01-22.mp3",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
@@ -24,9 +26,10 @@ function chillHop() {
     {
       name: "Keep Going",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "https://cms.chillhop.com/media/36728/squarelc0c6175640eb9aa14b955ae91e255b3175de1c32.jpg",
       artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/377/6576/main-version/streaming-previews/STREAMING-summer-love-vens-adams-main-version-02-33-19556.mp3",
       color: ["#CD607D", "#C45178"],
       id: uuidv4(),
       active: false,
@@ -34,9 +37,10 @@ function chillHop() {
     {
       name: "Cascade",
       cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+        "https://cms.chillhop.com/media/11854/square05f61e0e6867e30c9967efa624e9f4b564469e90.jpg",
       artist: "Knowmadic",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=13091",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/348/2724/main-version/streaming-previews/STREAMING-waves-goods-cargo-main-version-02-05-10187.mp3",
       color: ["#397484", "#7DCEA5"],
       id: uuidv4(),
       active: false,
@@ -44,9 +48,10 @@ function chillHop() {
     {
       name: "Ambleside",
       cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+        "https://cms.chillhop.com/media/4870/squareb74957163abad45d5cedaad42ea23691ffcb03e6.jpg",
       artist: "Aarigod",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12998",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/431/992/main-version/streaming-previews/STREAMING-beneath-the-surface-fugu-vibes-main-version-02-13-3391.mp3",
       color: ["#617746", "#B7D170"],
       id: uuidv4(),
       active: false,
@@ -54,9 +59,10 @@ function chillHop() {
     {
       name: "On The Moon",
       cover:
-        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+        "https://cms.chillhop.com/media/20386/square732128e1da8fd5f6292ffc1926a2ea840a54f4d0.jpg",
       artist: "SwuM",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=15233",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/161/1783/main-version/streaming-previews/STREAMING-aspire-all-good-folks-main-version-02-18-6252.mp3",
       color: ["#5e4629", "#985F36"],
       id: uuidv4(),
       active: false,
@@ -64,9 +70,10 @@ function chillHop() {
     {
       name: "Lonely Waves",
       cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+        "https://cms.chillhop.com/media/35631/square36fcc61f2d1c707e99f36706700d73229e2e8dff.jpg",
       artist: "C Y G N",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=14980",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/90460af8401a0c906aa65f33459c4428/57b70f074a63c13766a12df97cd066f7/38785f4360c4e194e8612fd584af6f46/STREAMING-natural-beauty-vens-adams-main-version-41-02-38.mp3",
       color: ["#B4DAFA", "#D784DE"],
       id: uuidv4(),
       active: false,
@@ -74,9 +81,10 @@ function chillHop() {
     {
       name: "Seascape",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+        "https://cms.chillhop.com/media/3775/squarebe9e5dca9d4c9015c407a2d2590af8a0e5e278ce.jpg",
       artist: "Makzo",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11772",
+      audio:
+        "https://cdn.uppbeat.io/audio-output/330/211/main-version/streaming-previews/STREAMING-polychrome-mood-maze-main-version-03-00-1046.mp3",
       color: ["#6174AE", "#E98C73"],
       id: uuidv4(),
       active: false,
@@ -84,9 +92,10 @@ function chillHop() {
     {
       name: "Not A Cloud In Sight",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
+        "https://cms.chillhop.com/media/1/square1bc92d5c86fb0c0d88bbd6b3f1a604512a1ebf55.jpg",
       artist: "Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10449",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/6fe37c64db62e03d698e462645fa35a7/fbbc2d98ce62ea1610bbee54fc512000/0fff417e0f5f4f46d479bade3e4d0450/STREAMING-good-night-breeze-pecan-pie-main-version-18943-02-28.mp3",
       color: ["#B767B0", "#64A2DF"],
       id: uuidv4(),
       active: false,
@@ -94,9 +103,10 @@ function chillHop() {
     {
       name: "Fushing the Stairs",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/6defa6c89a6902fdd84bccd720c2b8fd29c40990-1024x1024.jpg",
+        "https://cms.chillhop.com/media/2319/squaref5170130ca62e18fb3928feb3d851f3f07f77e18.jpg",
       artist: "Leavv",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9916",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/feb0842d36ded48702cb4038d4bc3c09/9a9a9962343b6d9b4f3ba7ec6bae73bd/8ae423fc62a429eb68ebb995cbea7256/STREAMING-malibop-lloom-main-version-25577-03-18.mp3",
       color: ["#04564E", "#6DA961"],
       id: uuidv4(),
       active: false,
@@ -104,9 +114,10 @@ function chillHop() {
     {
       name: "Imagination",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/24bd0da848d9718fd19af104ff5c0b11f914cb25-1024x1024.jpg",
+        "https://cms.chillhop.com/media/10266/square2899f7cc22ab12e17d0119819aac3ca9dbab46e6.jpg",
       artist: "Montell Fish",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=6795",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/b2810913f88b72f580ee1f44ab4ef849/940b59d02223fa8abef4d338f3a14daf/e4c6d7256366228380569f798e61841b/STREAMING-the-cleaner-night-drift-main-version-20732-02-01.mp3",
       color: ["#A05757", "#CE587B"],
       id: uuidv4(),
       active: false,
@@ -114,9 +125,10 @@ function chillHop() {
     {
       name: "Imagination",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+        "https://cms.chillhop.com/media/21805/squarea0dcd19cea6c0c6d056b45170013a2368a8ca0bf.jpg",
       artist: "Philanthrope, Sitting Duck, squeeda",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=6838",
+      audio:
+        "https://cdn.uppbeat.io/audio-files/d698e5b0e6235427c770929d50b27956/c3c9cae46b23e147a0bcd7a6ecf6141e/18be5a13f12e7e0e5c8c56de6b53a109/STREAMING-let-s-stay-in-braden-deal-main-version-26089-04-11.mp3",
       color: ["#6ECABB", "#F2D36A"],
       id: uuidv4(),
       active: false,
